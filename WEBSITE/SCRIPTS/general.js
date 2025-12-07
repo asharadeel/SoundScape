49d4ece04a800f75bb401f102dbccc9c8aa5df07
Ashar Adeel <asharadeel@outlook.com>
2025-12-07 02:42:40 +0000

    MOBILE VIEW BAN (TEST)
    testing to redirect pages if on mobile view, to redirect to mobile.html - a forbidden page

---
+++ WEBSITE/SCRIPTS/general.js
@@ -0,0 +1,18 @@
+// MOBILE REDIRECT HANDLER
+(function () {
+  const isMobileDevice =
+    /Android|iPhone|iPad|iPod|Windows Phone|BlackBerry|webOS/i.test(navigator.userAgent);
+
+  const isSmallScreen = window.innerWidth < 700;
+
+  // Get current page file name (ex: "index.html")
+  const currentPage = window.location.pathname.split("/").pop();
+
+  // Do NOT redirect if already on mobile.html
+  if (currentPage === "mobile.html") return;
+
+  // Redirect if on mobile or very small screen
+  if (isMobileDevice || isSmallScreen) {
+    window.location.href = "mobile.html";
+  }
+})();
